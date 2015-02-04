e22f086f764a7db27119762bdd69971f5dc2fa52
dhiraj-gangoosirdar-cko <dhiraj.gangoosirdar@checkout.com>
2015-02-04 16:05:25 +0400

    adding the admin setup page

--- app/code/community/Djs/DynamicCatalog/controllers/Adminhtml/DynamiccategoryController.php
+++ app/code/community/Djs/DynamicCatalog/controllers/Adminhtml/DynamiccategoryController.php
@@ -3,9 +3,22 @@ class Djs_DynamicCatalog_Adminhtml_DynamiccategoryController extends Mage_Adminh
 {
     public function indexAction()
     {
-        $this->_title($this->__('Locator'))
-            ->_title($this->__('Manage Locations'));
+        $this->_title($this->__('Catalog'))
+            ->_title($this->__('Manage Dynamic Catalog'));
 
-        $this->loadLayout()->renderLayout();
+        $this->_initAction()
+            ->_addBreadcrumb(Mage::helper('djs_dynamiccatalog')->__('Catalog'), Mage::helper('djs_dynamiccatalog')->__
+            ('Catalog'))
+            ->renderLayout();
+    }
+
+    protected function _initAction()
+    {
+        $this->loadLayout()
+            ->_setActiveMenu('catalog/dynamiccatalog')
+            ->_addBreadcrumb(Mage::helper('djs_dynamiccatalog')->__('Categories'), Mage::helper('djs_dynamiccatalog')
+                ->__('Categories'))
+        ;
+        return $this;
     }
 }
